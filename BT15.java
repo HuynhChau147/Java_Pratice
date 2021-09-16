46753bf3059f946185618fba9bb72e6f4c958ff9
HuynhChauTapCode <huynhchau8743@gmail.com>
2021-09-16 20:05:04 +0700

    BT15.java added

---
+++ BT15.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class BT15 {
+public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+	System.out.println("Input a number you want tranfer to binary code : ");
+	int a = input.nextInt();
+	Binary(a);
+}
+
+private static void Binary(int a) {
+	ArrayList<String> List = new ArrayList<>();
+	int d = a;
+	do
+	{
+		String b = Integer.toString(d%2) ;
+		List.add(b);
+		d = d/2;
+	}
+	while(d != 0);
+	System.out.println(a +" - "+ List);
+}
+}
